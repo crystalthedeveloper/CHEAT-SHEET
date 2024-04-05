c49893755c3824997e94d858497ced8a52ec67cf
crystallewisdeveloper <107262091+crystallewisdeveloper@users.noreply.github.com>
2024-04-04 21:36:26 -0400

    mysql added

--- database-backends.py
+++ database-backends.py
@@ -1,6 +1,17 @@
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+#
+# Database
+#
+
+# Install MySQL Client:
+# ~$ pipenv install mysqlclient 
+# ~$ pip3 install mysqlclient
+# ~$ pip3 install pymysql
+
+# Installs psycopg2-binary, which is a PostgreSQL adapter for Python. The version number 2.10.0
+# ~$ pipenv install psycopg2-binary==2.10.0
 
 # sqlite3
 DATABASES = {
